6543e972c6f2cf047555c06f085a83338c455e81
Muhammad Talha Bin Mansoor <m.talhabinmansoor@gmail.com>
2020-12-10 21:50:04 +0500

    Update

---
+++ scripts/DBServer.py
@@ -0,0 +1,42 @@
+import pyodbc 
+from flask import Flask, jsonify
+
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=DESKTOP-P75CUH1\SQLEXPRESS;'
+                      'Database=AdventureWorksDW2019;'
+                      'Trusted_Connection=yes;')
+
+
+cursor = conn.cursor()
+
+# cursor.execute('SELECT TOP (100) [CustomerKey], [FirstName], [MiddleName], [LastName] FROM [AdventureWorksDW2019].[dbo].[DimCustomer]')
+
+# names = []
+# lastnames = []
+# for row in cursor:
+#     names.append({row.CustomerKey: row.FirstName})
+#     lastnames.append({row.CustomerKey: row.LastName})
+     
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def hello_wolrd():
+    return jsonify(names)
+
+def query1():
+    cursor.execute('SELECT TOP (100) [CustomerKey], [FirstName], [MiddleName], [LastName] FROM [AdventureWorksDW2019].[dbo].[DimCustomer]')
+
+
+@app.route('/last/')
+
+def last_names():
+
+    return jsonify(lastnames)
+
+
+if __name__ == '__main__':
+   app.run()
+
+
