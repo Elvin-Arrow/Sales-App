f83b012e7f2d6dc6318a90e21405b335f5222739
Elvin Arrow <m.talhabinmansoor@gmail.com>
2020-12-12 12:15:56 +0500

    Add bloc provider

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sales_app/src/Ui/home_page.dart';
+import 'package:sales_app/src/cubit/page_cubit.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MultiBlocProvider(providers: null, child: HomePage()),
+      home: MultiBlocProvider(providers: [
+        BlocProvider(
+          create: (_) => PageCubit(),
+        ),
+      ], child: HomePage()),
     );
   }
 }
